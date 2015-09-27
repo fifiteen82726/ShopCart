b67c359154ccc1367f0ce36a3b857baacf0091dc
fifiteen82726 <fifiteen82726@gmail.com>
2015-09-28 01:44:45 +0800

    modifying migration, 29:24

---
+++ db/migrate/20150927173603_init_shop.rb
@@ -0,0 +1,36 @@
+class InitShop < ActiveRecord::Migration
+  def change
+  	create_table :cates do |t|
+  		t.string :name
+  		t.integer :position
+  		t.timestamps
+
+  	end
+  	create_table :items do |t|
+  		t.string :name
+  		t.integer :price
+  		t.text :descript
+  		t.timestamps
+  		t.timestamps :delete_at
+
+  end
+
+  	create_table :orders do |t|
+  		t.integer :user_id
+  		t.timestamps
+  		t.integer :status, :limit => 1, :null => false, :default => 0
+
+  	end
+
+  	create_table :order_items do |t|
+  		t.integer :order_id
+  		t.integer :item_id
+  		t,integer :user_id
+  		t.integer :price, :null => false
+	end
+
+
+end
+
+end
+
