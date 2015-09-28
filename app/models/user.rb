066ffca36f3788b1f2e5f268cad50b574b219779
fifiteen82726 <fifiteen82726@gmail.com>
2015-09-28 17:41:08 +0800

    remove user has many, modify order_itme.rb name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :order_items
+  #has_many :order_items
   #如果沒用到的就直接關掉
   devise :database_authenticatable, :registerable
          #:recoverable, :rememberable, :trackable, :validatable
